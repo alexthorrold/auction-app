af2744471b381f8f88414d729abb61ef9c936f67
alexthorrold <91945780+alexthorrold@users.noreply.github.com>
2024-02-21 20:14:59 +1300

    Set up auction service

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,34 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+});
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+{
+	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+	DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex);
+}
+
+app.Run();
